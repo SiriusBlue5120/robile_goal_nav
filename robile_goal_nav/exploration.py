ed1c053665435cd8ba55e3028dfdc1deab155b41
Shinas Shaji <shinasshaji12@outlook.com>
2024-03-08 21:20:56 +0100

    Bug fixes

--- robile_goal_nav/exploration.py
+++ robile_goal_nav/exploration.py
@@ -3,14 +3,14 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import  Pose, PointStamped,PoseStamped
+from geometry_msgs.msg import  Pose, PointStamped, PoseWithCovarianceStamped
 from geometry_msgs.msg import TransformStamped
 import tf_transformations as tf
 import tf2_ros
 
 class Exploration(Node):
 
-    def __init__(self,args=None):        
+    def __init__(self, behavior=False):        
         super().__init__(node_name="Exploration")
         # Logging
         self.verbose = True
@@ -35,26 +35,26 @@ class Exploration(Node):
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
         # subscription for map
-        self.scan_subscriber = self.create_subscription(
-            OccupancyGrid, "/map",
-            self.get_occupancy_grid,
-            qos_profile=rclpy.qos.qos_profile_sensor_data
-        )
+        # self.scan_subscriber = self.create_subscription(
+        #     OccupancyGrid, "/map",
+        #     self.get_occupancy_grid,
+        #     qos_profile=rclpy.qos.qos_profile_sensor_data
+        # ) if not behavior else None
 
         # Subscription to robot pose
 
-        self.pose_subscriber = self.create_subscription(
-            PoseStamped,
-            "/pose",
-            self.pose_callback,
-            10
-        ) 
+        # self.pose_subscriber = self.create_subscription(
+        #     PoseWithCovarianceStamped,
+        #     "/pose",
+        #     self.pose_callback,
+        #     10
+        # ) if not behavior else None
 
         # publisher for pose
-        self.pose_next = PointStamped()
         self.pose_publisher = self.create_publisher(
             PointStamped, '/pose_explore',
             10)
+        self.pose_next = PointStamped()
         
         # Flags
         self.goal_explored = True
@@ -82,23 +82,22 @@ class Exploration(Node):
         self.data_grid = np.array(msg.data, dtype=np.int64).reshape(\
             (self.height, self.width), order='F')
 
-
         # data_in_2d = self.data_grid.reshape((self.height, self.width), order='F')
 
+        # if self.goal_explored or self.validation:
+        self.calculate_fringe_pose()
+        self.pose_next.header.frame_id = self.odom_frame
+        self.pose_next.header.stamp = msg.header.stamp
 
-        if self.goal_explored or self.validation:
-            self.calculate_fringe_pose()
-            self.pose_next.header.frame_id = self.odom_frame
-            self.pose_next.header.stamp = msg.header.stamp
+        self.pose_publisher.publish(self.pose_next)
 
-            self.pose_publisher.publish(self.pose_next)
+        self.reset_goal_explored()
 
-            self.reset_goal_explored()
 
-    def pose_callback(self,msg:PoseStamped) -> None:
+    def pose_callback(self,msg:PoseWithCovarianceStamped) -> None:
 
-        robot_x = msg.pose.position.x
-        robot_y = msg.pose.position.y
+        robot_x = msg.pose.pose.position.x
+        robot_y = msg.pose.pose.position.y
 
         self.robot_pose = (robot_x,robot_y)
 
@@ -140,18 +139,18 @@ class Exploration(Node):
 
         point_list = np.array(((point_list)))
 
-        diference_list = point_list - self.robot_pos
+        diference_list = point_list - self.robot_pose
         norm_vec = np.linalg.norm(diference_list,axis=1)
         min_value_idx = np.argmin(norm_vec)
 
-        self.pose_next = point_list[min_value_idx]
+        point_next = point_list[min_value_idx]
 
         if self.verbose:
             print('------------')
-            print(random_point)
+            print(point_next)
 
-        self.pose_next.point.x = random_point[0]*self.resolution + self.origin.position.x
-        self.pose_next.point.y = random_point[1]*self.resolution + self.origin.position.y
+        self.pose_next.point.x = point_next[0]*self.resolution + self.origin.position.x
+        self.pose_next.point.y = point_next[1]*self.resolution + self.origin.position.y
         # self.pose_next.pose.position.z = self.random_point[2]*self.resolution + self.origin.position.z
 
 
